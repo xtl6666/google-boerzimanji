07913145e928db726b27ed8ff4b9c5f719fc44e1
xtl6666 <13379085514@163.com>
2026-01-13 14:37:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // <--- 重点：加上这一行！
+    
     server: {
       port: 3000,
       host: '0.0.0.0',
